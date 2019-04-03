61b5eae22c4da87ef6e1dae8b236327bd30adf84
paul <paul@Mac-Pro-Opale.local>
2019-04-03 16:03:39 +0200

    Fis routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
     root to: "users#index"#user page
+    resources :users, only: [:index]
 
     resources :subscriptions, only: [:follow, :unfollow, :show]
-
-    get 'tweets_page' => 'tweets#tweets_page'# page of all tweets
-    post 'tweets_page' => 'tweets#create'# creation of a tweet
-
     post 'follow' => 'subscriptions#follow'# follow a user
     post 'unfollow' => 'subscriptions#unfollow'# unfollow a user
     get 'portal_show'  => 'subscriptions#show'
 
     resources :tweets, only: [:tweets_page, :create]
-    resources :users, only: [:index]
+    get 'tweets_page' => 'tweets#tweets_page'# page of all tweets
+    post 'tweets_page' => 'tweets#create'# creation of a tweet
+    
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
