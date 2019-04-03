7d656c5a17d9cf111299731fa7d8560064c30a19
paul <paul@Mac-Pro-Opale.local>
2019-04-03 14:48:18 +0200

    Delete Portal controller (last commit was the model)

--- test/controllers/portal_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class PortalControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get portal_index_url
-    assert_response :success
-  end
-
-end
