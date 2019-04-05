12080870450bd7b7f49e481fdfc8887f5e0106c4
Poulpy <paul.repain@yahoo.com>
2019-04-05 11:14:10 +0200

    Scaffold Reply destroyed; next : resolve 'no route matches delete /users/destroy

--- app/models/reply.rb
+++
@@ -1,4 +0,0 @@
-class Reply < ApplicationRecord
-  belongs_to :parent_tweet, class_name: "Tweet", optional: true
-  belongs_to :child_tweet, class_name: "Tweet", optional: true
-end
--- app/views/replies/_reply.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! reply, :id, :parent_id, :child_id, :created_at, :updated_at
-json.url reply_url(reply, format: :json)
