4778eb6854bb726a753e699f886d54d771db76e7
Poulpy <paul.repain@yahoo.com>
2019-04-05 14:36:36 +0200

    Link to view profile + link to settings (from profile page); next : tweet box with bootstrap

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_user, only: [:show]
+  before_action :set_user, only: [:index, :show]
+
+
+  def index
+  end
 
 
   # GET /users/1
@@ -12,7 +16,7 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:id])
+      #@user = User.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:show]
+  resources :users, only: [:index, :show]
   root to: "tweets#index"
 
   resources :replies, only: [:index]
