a805fdad510d5ae9d0b1b81a7d739707e9268b1d
Poulpy <paul.repain@yahoo.com>
2019-05-31 15:56:18 +0200

    Added seeds for db in seeds.rb (for deployment)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(name: "admin", password: "admin", email: "admin@admin.fr")
+User.create(name: "user", password: "user", email: "user@user.fr")
